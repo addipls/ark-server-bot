2bf56dcbee4f622aad2cd3a0349123898317bf5f
addi <72429964+addipls@users.noreply.github.com>
2022-12-08 17:35:29 -0500

    Try waiting

--- commands/checkformodupdates.js
+++ commands/checkformodupdates.js
@@ -1,19 +1,21 @@
 const { execSync } = require('child_process');
 const { SlashCommandBuilder } = require('discord.js');
-const { runCommand } = require('../util/SystemCommandHandler.js');
+const {runCommand} = require('../util/SystemCommandHandler.js');
 const { Constants } = require('../util/Constants.js');
+
+const wait = require('node:timers/promises').setTimeout;
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('checkformodupdates')
-		.setDescription('Checks if any mods need to be updated'),
+		.setDescription('Checks for mod updates'),
 	async execute(interaction) {
         await interaction.deferReply();
         let response = runCommand(Constants.get('CHECK_FOR_MOD_UPDATES'));
+        wait(2500);
         console.log(response);
 		await interaction.editReply(
             {
                 content: response,
-                ephemeral: false,
             }
         );
 	},
