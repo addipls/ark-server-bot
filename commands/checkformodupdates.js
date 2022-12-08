ed74e685aa5133770ebc97b75e46a468e5683880
addi <72429964+addipls@users.noreply.github.com>
2022-12-08 17:45:23 -0500

    Different mechanic

--- commands/checkformodupdates.js
+++ commands/checkformodupdates.js
@@ -1,22 +1,23 @@
 const { execSync } = require('child_process');
 const { SlashCommandBuilder } = require('discord.js');
-const {runCommand} = require('../util/SystemCommandHandler.js');
+const { runCommand } = require('../util/SystemCommandHandler.js');
 const { Constants } = require('../util/Constants.js');
-
-const wait = require('node:timers/promises').setTimeout;
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('checkformodupdates')
-		.setDescription('Checks for mod updates'),
+		.setDescription('Checks if an update to the Ark mods is available'),
 	async execute(interaction) {
         await interaction.deferReply();
         let response = runCommand(Constants.get('CHECK_FOR_MOD_UPDATES'));
-        wait(2500);
         console.log(response);
+        await interaction.followUp('Using this instead of editReply');
+        /*
 		await interaction.editReply(
             {
                 content: response,
+                ephemeral: false,
             }
         );
+        */
 	},
 };
