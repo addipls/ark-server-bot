9126e48583d66c0762bd481f8582516e4a3651cd
addi <72429964+addipls@users.noreply.github.com>
2022-12-08 17:56:51 -0500

    Await the def, but not edit

--- commands/checkformodupdates.js
+++ commands/checkformodupdates.js
@@ -7,7 +7,7 @@ module.exports = {
 		.setName('checkformodupdates')
 		.setDescription('Checks if an update to the Ark mods is available'),
 	async execute(interaction) {
-        interaction.deferReply();
+        await interaction.deferReply();
         let response = runCommand(Constants.get('CHECK_FOR_MOD_UPDATES'));
         console.log(response);
 		interaction.editReply(
