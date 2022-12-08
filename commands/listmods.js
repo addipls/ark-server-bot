f0ebc04891208c0989381c13477ca71c7b182843
addi <72429964+addipls@users.noreply.github.com>
2022-12-08 16:55:50 -0500

    Attempt to defer reply

--- commands/listmods.js
+++ commands/listmods.js
@@ -2,14 +2,17 @@ const { execSync } = require('child_process');
 const { SlashCommandBuilder } = require('discord.js');
 const {runCommand} = require('../util/SystemCommandHandler.js');
 const { Constants } = require('../util/Constants.js');
+
+const wait = require('node:timers/promises').setTimeout;
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('listmods')
 		.setDescription('Lists all mods installed on the server'),
 	async execute(interaction) {
+        await interaction.deferReply();
         let response = runCommand(Constants.get('LIST_MODS'));
         console.log(response);
-		interaction.reply(
+		await interaction.editReply(
             {
                 content: response,
                 ephemeral: false,
